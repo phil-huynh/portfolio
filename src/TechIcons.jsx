ac990e74a67836daa0ee0260bdafa5ec4067a1fa
phil-huynh <philip.huynh@galvanize.com>
2023-11-09 22:42:03 -0500

    completed converting currently displayed icons to React components

--- src/TechIcons.jsx
+++ src/TechIcons.jsx
@@ -1,44 +1,3 @@
-import pythonLogo from './assets/tech_icons/python.svg'
-import javascriptLogo from './assets/tech_icons/javascript.svg'
-import reactLogo from './assets/tech_icons/react.svg'
-import djangoLogo from './assets/tech_icons/django.svg'
-import nodeLogo from './assets/tech_icons/node.svg'
-import fastAPILogo from './assets/tech_icons/fastapi.svg'
-import postgresLogo from './assets/tech_icons/postgres.svg'
-import mongoLogo from './assets/tech_icons/mongo.svg'
-import mySQLLogo from './assets/tech_icons/mysql.svg'
-import sqliteLogo from './assets/tech_icons/sqlite.svg'
-import typescriptLogo from './assets/tech_icons/typescript.svg'
-import webpackLogo from './assets/tech_icons/webpack.svg'
-import babelLogo from './assets/tech_icons/babel.svg'
-import awsLogo from './assets/tech_icons/aws.svg'
-import bashLogo from './assets/tech_icons/bash.svg'
-import cssLogo from './assets/tech_icons/css.svg'
-import dockerLogo from './assets/tech_icons/docker.svg'
-import reduxLogo from './assets/tech_icons/redux.svg'
-import redisLogo from './assets/tech_icons/redis.svg'
-import mochaLogo from './assets/tech_icons/mocha.svg'
-import jestLogo from './assets/tech_icons/jest.svg'
-import pytestLogo from './assets/tech_icons/pytest.svg'
-import jqueryLogo from './assets/tech_icons/jquery.svg'
-import linuxLogo from './assets/tech_icons/linux.svg'
-import markdownLogo from './assets/tech_icons/markdown.svg'
-import nginxLogo from './assets/tech_icons/nginx.svg'
-import npmLogo from './assets/tech_icons/npm.svg'
-import bootstrapLogo from './assets/tech_icons/bootstrap.svg'
-import bulmaLogo from './assets/tech_icons/bulma.svg'
-import expressLogo from './assets/tech_icons/express.svg'
-import figmaLogo from './assets/tech_icons/figma.svg'
-import herokuLogo from './assets/tech_icons/heroku.svg'
-import htmlLogo from './assets/tech_icons/html.svg'
-import jasmineLogo from './assets/tech_icons/jasmine.svg'
-import materialUILogo from './assets/tech_icons/materialui.svg'
-import nextLogo from './assets/tech_icons/next.svg'
-import sassLogo from './assets/tech_icons/sass.svg'
-import numpyLogo from './assets/tech_icons/numpy.svg'
-import flaskLogo from './assets/tech_icons/flask.svg'
-import jiraLogo from './assets/tech_icons/jira.svg'
-import viteLogo from '/vite.svg'
 import AWSLogo from './tech_icons/AmazonWebServices'
 import HTMLLogo from './tech_icons/HTML_Logo'
 import CSSLogo from './tech_icons/CSS_Logo'
@@ -54,6 +13,27 @@ import BootstrapIcon from './tech_icons/BootstrapIcon'
 import BulmaIcon from './tech_icons/BulmaIcon'
 import MaterialUIIcon from './tech_icons/MaterialUIIcon'
 import FigmaIcon from './tech_icons/FigmaIcon'
+import NodeIcon from './tech_icons/NodeIcon'
+import DjangoIcon from './tech_icons/DjangoIcon'
+import FastAPIIcon from './tech_icons/FastAPIIcon'
+import FlaskIcon from './tech_icons/FlaskIcon'
+import LinuxIcon from './tech_icons/LinuxIcon'
+import DockerIcon from './tech_icons/DockerIcon'
+import NGNIXIcon from './tech_icons/NGNIXIcon'
+import PostgresIcon from './tech_icons/PostgresIcon'
+import MySQLIcon from './tech_icons/MySQLIcon'
+import SQLiteIcon from './tech_icons/SQLiteIcon'
+import MongoIcon from './tech_icons/MongoIcon'
+import RedisIcon from './tech_icons/RedisIcon'
+import JasmineIcon from './tech_icons/JasmineIcon'
+import MochaIcon from './tech_icons/MochaIcon'
+import JestIcon from './tech_icons/JestIcon'
+import PytestIcon from './tech_icons/PytestIcon'
+import BashIcon from './tech_icons/BashIcon'
+import WebpackIcon from './tech_icons/Webpack'
+import BabelIcon from './tech_icons/BabelIcon'
+import ViteIcon from './tech_icons/ViteIcon'
+import MarkdownIcon from './tech_icons/MarkdownIcon'
 
 export default function TechIcons() {
   const iconContainerStyle = {
@@ -73,11 +53,6 @@ export default function TechIcons() {
     "marginTop": "1rem"
   }
 
-  const iconStyle = {
-    "display": "flex",
-    "placeContent": "center",
-    // "border": "solid black 1px"
-  }
 
   return (
     <div className="svg-shadow" style={iconContainerStyle}>
@@ -101,185 +76,38 @@ export default function TechIcons() {
         <BulmaIcon/>
         <MaterialUIIcon/>
         <FigmaIcon/>
-        {/* <div title="Material UI" className="svg-shadow" style={{...iconStyle, "width": '5rem'}}>
-          <img src={materialUILogo}></img>
-        </div> */}
-        {/* <div title="Figma" className="svg-shadow" style={{...iconStyle, "width": '5rem'}}>
-          <img src={figmaLogo}></img>
-        </div> */}
       </section>
       <section className="svg-shadow" style={iconSectionStyle}>
-
-        <div
-          title="node js"
-          className="svg-shadow" style={{
-            "display": "flex",
-            "alignItems": "center",
-            "height": "2.8rem",
-            "overflowY": "hidden",
-            // "backgroundColor": "white",
-            // "borderRadius": "10px"
-          }}
-        >
-          <div className="svg-shadow" style={{...iconStyle, "width": '8rem'}}>
-            <img src={nodeLogo} className="svg-shadow" style={{"width": "8rem"}}></img>
-          </div>
-        </div>
-        <div
-          title="django"
-          className="svg-shadow"
-          style={{
-            "display": "flex",
-            "alignItems": "center",
-            "height": "4rem",
-            "overflowY": "hidden",
-          }}
-        >
-          <div className="svg-shadow" style={{...iconStyle, "width": '6rem'}}>
-            <img src={djangoLogo} className="svg-shadow" style={{"width": "6rem"}}></img>
-          </div>
-        </div>
-
-        <div
-          title="FastAPI"
-          className="svg-shadow" style={{
-            "display": "flex",
-            "alignItems": "center",
-            "height": "4rem",
-            "overflowY": "hidden",
-          }}>
-          <div className="svg-shadow" style={{...iconStyle, "width": '8rem'}}>
-            <img src={fastAPILogo} className="svg-shadow" style={{"width": "8rem"}}></img>
-          </div>
-        </div>
-        <div
-          title="Flask"
-          className="svg-shadow" style={{
-            "display": "flex",
-            "alignItems": "center",
-            "height": "3.3rem",
-            "overflowY": "hidden",
-            // "backgroundColor": "white",
-            "borderRadius": "10px"
-          }}>
-          <div className="svg-shadow" style={{...iconStyle, "width": '7.2rem'}}>
-            <img src={flaskLogo} className="svg-shadow" style={{"width": "7rem"}}></img>
-          </div>
-        </div>
+        <NodeIcon/>
+        <DjangoIcon/>
+        <FastAPIIcon/>
+        <FlaskIcon/>
       </section>
       <section className="svg-shadow" style={iconSectionStyle}>
-        <div title="Postgres" className="svg-shadow" style={{...iconStyle, "width": '5rem'}}>
-          <img src={postgresLogo}></img>
-        </div>
-        <div title="MySQL" className="svg-shadow" style={{...iconStyle, "width": '5rem'}}>
-          <img src={mySQLLogo}></img>
-        </div>
-        <div title="SQLite" className="svg-shadow" style={{...iconStyle, "width": '5rem'}}>
-          <img src={sqliteLogo}></img>
-        </div>
-        <div title="MongoDB" className="svg-shadow" style={{...iconStyle, "width": '5rem'}}>
-          <img src={mongoLogo}></img>
-        </div>
-        <div title="Redis" className="svg-shadow" style={{...iconStyle, "width": '5rem'}}>
-          <img src={redisLogo}></img>
-        </div>
+        <PostgresIcon/>
+        <MySQLIcon/>
+        <SQLiteIcon/>
+        <MongoIcon/>
+        <RedisIcon/>
       </section>
       <section className="svg-shadow" style={iconSectionStyle}>
-        <div title="Jasmine" className="svg-shadow" style={{...iconStyle, "width": '5rem'}}>
-          <img src={jasmineLogo}></img>
-        </div>
-        <div title="Mocha" className="svg-shadow" style={{...iconStyle, "width": '5rem'}}>
-          <img src={mochaLogo}></img>
-        </div>
-        <div title="Jest" className="svg-shadow" style={{...iconStyle, "width": '5rem'}}>
-          <img src={jestLogo}></img>
-        </div>
-        <div title="pytest" className="svg-shadow" style={{...iconStyle, "width": '5rem'}}>
-          <img src={pytestLogo}></img>
-        </div>
-        <div
-          className="svg-shadow" >
-          <div title="BASH" className="svg-shadow" style={{...iconStyle, "width": '5rem'}}>
-            <img src={bashLogo}></img>
-          </div>
-        </div>
+        <JasmineIcon/>
+        <MochaIcon/>
+        <JestIcon/>
+        <PytestIcon/>
+        <BashIcon/>
       </section>
       <section className="svg-shadow" style={iconSectionStyle}>
-        {/* <div
-          title="Amazon Web Services"
-          className="svg-shadow"
-        >
-          <div className="svg-shadow" style={{...iconStyle, "width": '10rem'}}>
-            <img src={awsLogo} className="svg-shadow" style={{"width": '10rem'}}></img>
-          </div>
-        </div> */}
         <AWSLogo/>
-        <div title="Linux" className="svg-shadow" style={{...iconStyle, "width": '5rem'}}>
-          <img src={linuxLogo}></img>
-        </div>
-        <div title="Docker" className="svg-shadow" style={{...iconStyle, "width": '5rem'}}>
-          <img src={dockerLogo}></img>
-        </div>
-        <div
-          title="ngnix"
-          className="svg-shadow" style={{
-            "display": "flex",
-            "alignItems": "center",
-            "height": "4.8rem",
-            "overflowY": "hidden",
-          }}
-        >
-          <div className="svg-shadow" style={{...iconStyle, "width": '7rem', 'paddingTop': '3.3rem'}}>
-            <img src={nginxLogo} className="svg-shadow" style={{"width": '7rem'}}></img>
-          </div>
-        </div>
-
-        {/* <div className="svg-shadow" style={{...iconStyle, "width": '5rem'}}>
-          <img src={herokuLogo}></img>
-        </div> */}
+        <LinuxIcon/>
+        <DockerIcon/>
+        <NGNIXIcon/>
       </section>
-
       <section className="svg-shadow" style={iconSectionStyle}>
-        <div
-          title="Webpack"
-          className="svg-shadow" style={{
-            "display": "flex",
-            "alignItems": "center",
-            "height": "4rem",
-            "overflowY": "hidden",
-          }}
-        >
-          <div className="svg-shadow" style={{...iconStyle, "width": '10rem'}}>
-            <img src={webpackLogo} className="svg-shadow" style={{"width": "10rem"}}></img>
-          </div>
-        </div>
-        <div
-          title="Babel"
-          className="svg-shadow" style={{
-            "display": "flex",
-            "alignItems": "center",
-            "height": "4rem",
-            "overflowY": "hidden",
-          }}
-        >
-          <div className="svg-shadow" style={{...iconStyle, "width": '7rem'}}>
-            <img src={babelLogo} className="svg-shadow" style={{"width": "7rem"}}></img>
-          </div>
-        </div>
-        <div
-          title="Vite"
-          className="svg-shadow" style={{...iconStyle, "width": '3.5rem'}}
-        >
-          <img src={viteLogo} className="svg-shadow" style={{"width": "3.5rem"}}></img>
-        </div>
-        <div
-          title="Markdown"
-          className="svg-shadow"
-        >
-          <div title="Markdown" className="svg-shadow" style={{...iconStyle, "width": '4.3rem'}}>
-            <img src={markdownLogo} className="svg-shadow" style={{"width": "4rem"}}></img>
-          </div>
-        </div>
+        <WebpackIcon/>
+        <BabelIcon/>
+        <ViteIcon/>
+        <MarkdownIcon/>
       </section>
       </div>
     </div>
---
+++ src/tech_icons/ViteIcon.jsx
@@ -0,0 +1,21 @@
+import viteLogo from '/vite.svg'
+
+export default function ViteIcon() {
+
+  const iconStyle = {
+    "display": "flex",
+    "placeContent": "center",
+    // "border": "solid black 1px"
+  }
+
+  return (
+    <div
+      title="Vite"
+      className="svg-shadow" style={{...iconStyle, "width": '3.5rem'}}
+    >
+      <img src={viteLogo} className="svg-shadow" style={{"width": "3.5rem"}}></img>
+    </div>
+  )
+}
+
+// xmlns:xlink="http://www.w3.org/1999/xlink"
