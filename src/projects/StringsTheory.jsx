bf062bcd9adc83054ef1c6b503eb5ccc88c12e4c
phil-huynh <philip.huynh@galvanize.com>
2023-11-15 17:05:31 -0500

    updated icons

--- src/projects/StringsTheory.jsx
+++ src/projects/StringsTheory.jsx
@@ -31,18 +31,18 @@ export default function StringsTheory() {
   // }
 
   const images = [
-    mapScalesPage,
-    selectedNoteNoChord,
-    selectedNoteWithChord,
-    oneChordInMapScales,
+    // mapScalesPage,
+    // selectedNoteNoChord,
+    // selectedNoteWithChord,
+    // oneChordInMapScales,
     twoChordsMapScales,
     focusChord1MapScales,
     focusChord2MapScales,
     chord1DegsMapScales,
     chord2DegsMapScales,
-    capo,
-    sectionNeck,
-    leftHanded,
+    // capo,
+    // sectionNeck,
+    // leftHanded,
     alterations,
     scaleChoices,
     instrumentsAndTunings,
@@ -54,16 +54,23 @@ export default function StringsTheory() {
   const navigate = useNavigate()
 
   const headerStyle = {
-    "height": "5rem",
     "width": "45%",
     "display": "flex",
     "alignItems": "center",
     "justifyContent": "center"
   }
+  const infoBoxStyle = {
+    "width": "75%",
+    "display": "flex",
+    "flexDirection": "column",
+    "alignItems": "center",
+    "justifyContent": "center",
+    "padding": "2rem"
+  }
 
   const imageStyle = {
     "backgroundSize": "cover",
-    "width": "95%",
+    "width": "80%",
     "margin": "1rem",
     "aspectRatio": "6.75/4"
   }
@@ -71,9 +78,27 @@ export default function StringsTheory() {
   const imageStyle2 = {
     "backgroundSize": "cover",
     "backgroundPosition": "center",
-    "width": "90rem",
-    "height": "17.5rem",
-    "margin": "1rem"
+    "width": "90%",
+    "margin": "1rem",
+    "aspectRatio": "11.2/2.25"
+  }
+
+  const introStyle = {
+    "height": "90vh",
+    "width": "100vw",
+    "display": "flex",
+    "flexDirection": "column",
+    "alignItems": "center",
+    "justifyContent": "center",
+    "gap": "2rem"
+  }
+
+  const groupStyle = {
+    "width": "75%",
+    "display": "flex",
+    "alignItems": "center",
+    "justifyContent": "center",
+    "padding": "2rem"
   }
 
   return (
@@ -86,7 +111,7 @@ export default function StringsTheory() {
     >
       <div className="strings-overlay"></div>
       <div className="contents">
-        <div style={{ "display": "flex", "alignItems": "center", "flexDirection": "column"}}>
+        {/* <div style={{ "display": "flex", "alignItems": "center", "flexDirection": "column"}}>
           <div className="glass" style={headerStyle}>
             <h1 style={{"color": "white"}}>Hello World: Strings Theory</h1>
           </div>
@@ -97,10 +122,39 @@ export default function StringsTheory() {
             <h2>Test</h2>
             <h2>Test</h2>
           </div>
+        </div> */}
+        <div style={introStyle}>
+          <div className="glass" style={headerStyle}>
+            <h1 style={{"color": "white"}}>Strings Theory</h1>
+          </div>
+          <div className="glass" style={headerStyle}>
+            <h2 style={{"cursor": 'pointer'}} onClick={()=>navigate('/')}>Home</h2>
+          </div>
+          <div className="glass" style={infoBoxStyle} >
+            <h2>View the deployed application here</h2>
+            <a style={headerStyle} href="http://stringstheory.net">
+              <h2 style={{"cursor": 'pointer', "color": "white"}} onClick={()=>navigate('/')}>stringstheory.net</h2>
+            </a>
+          </div>
         </div>
         <div style={{"display": "flex", "justifyContent": "space-around",  "flexWrap": "wrap"}}>
-          <div style={{...imageStyle2, "backgroundImage": `url(${scaleDegrees})`}}></div>
-          <div style={{...imageStyle2, "backgroundImage": `url(${solfege})`}}></div>
+          <div className="glass" style={infoBoxStyle}>
+            <div style={groupStyle}>
+              <div style={{...imageStyle, "backgroundImage": `url(${mapScalesPage})`}} />
+              <div style={{...imageStyle, "backgroundImage": `url(${leftHanded})`}} />
+            </div>
+            <div style={{...imageStyle2, "backgroundImage": `url(${scaleDegrees})`}}></div>
+            <div style={{...imageStyle2, "backgroundImage": `url(${solfege})`}}></div>
+          </div>
+          <div className="glass3" style={infoBoxStyle}>
+            <div style={{...imageStyle, "backgroundImage": `url(${selectedNoteNoChord})`}} />
+            <div style={{...imageStyle, "backgroundImage": `url(${selectedNoteWithChord})`}}></div>
+          </div>
+          <div className="glass4" style={infoBoxStyle}>
+            <div style={{...imageStyle, "backgroundImage": `url(${oneChordInMapScales})`}} />
+            <div style={{...imageStyle, "backgroundImage": `url(${sectionNeck})`}}></div>
+            <div style={{...imageStyle, "backgroundImage": `url(${capo})`}}></div>
+          </div>
           <Grid container>
           {images.map((image, i) => (
               <Grid
